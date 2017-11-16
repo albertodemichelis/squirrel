09828b8b059e04645b7135ab9185356cadc2093e
lindqva <anders.lindqvist@autodesk.com>
2017-11-16 22:25:32 +0100

    Prototype a system to automaticall test all C-examples in documentation.

---
+++ doc/source/reference/embedding/calling_a_function_example_1.h
@@ -0,0 +1,9 @@
+sq_pushroottable(v);
+sq_pushstring(v,"foo",-1);
+sq_get(v,-2); //get the function from the root table
+sq_pushroottable(v); //'this' (function environment object)
+sq_pushinteger(v,1);
+sq_pushfloat(v,2.0);
+sq_pushstring(v,"three",-1);
+sq_call(v,4,SQFalse);
+sq_pop(v,2); //pops the roottable and the function
---
+++ sqtestdoc/build_documentation.c
@@ -0,0 +1,17 @@
+#include <squirrel.h>
+#include <sqstdblob.h>
+#include <sqstdsystem.h>
+#include <sqstdio.h>
+#include <sqstdmath.h>
+#include <sqstdstring.h>
+#include <sqstdaux.h>
+
+static void reference_embedding_creating_a_c_function_example_1(HSQUIRRELVM v)
+{
+	#include "reference/embedding/references_from_c_example_1.h"
+}
+
+static void reference_embedding_calling_a_function_example_1(HSQUIRRELVM v)
+{
+	#include "reference/embedding/calling_a_function_example_1.h"
+}
