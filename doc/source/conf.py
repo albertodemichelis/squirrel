ca2c74b319ce379a00b646750bbbcd6cb81a39ed
albertodemichelis <alberto@demichelis.net>
2019-03-12 21:59:59 +0800

    removed unused extension from sphinx config

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,8 +29,7 @@ import time
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.pngmath',
+extensions = [    
 ]
 
 # Add any paths that contain templates here, relative to this directory.
