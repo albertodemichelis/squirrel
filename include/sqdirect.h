e36e70c3b5225fe52d78b017d92564215a881eab
VasiliyRyabtsev <vasiliy.ryabtsev@gmail.com>
2019-07-13 13:41:52 +0300

    Add 'direct' (non-stack-based) object manipulations API
    More optimal native API without unnecessary use of stack and refcount

---
+++ include/sqdirect.h
@@ -0,0 +1,25 @@
+/*
+ * Squirrel API extensions
+ * Copyright (C) 2003-2016  Gaijin Entertainment Corp.  All rights reserved
+ *
+ * Manipulate with Squirrel objects directlywithout unnecessary use of
+ * stack and refcount
+ */
+
+#ifndef _SQDIRECT_H_
+#define _SQDIRECT_H_
+
+
+SQUIRREL_API SQRESULT sq_direct_get(HSQUIRRELVM v, const HSQOBJECT *obj, const HSQOBJECT *slot,
+                                    HSQOBJECT *out, bool raw);
+
+#define   sq_direct_tointeger   sq_objtointeger
+#define   sq_direct_tofloat     sq_objtofloat
+
+SQUIRREL_API SQBool sq_direct_tobool(const HSQOBJECT *o);
+
+SQUIRREL_API SQBool sq_direct_cmp(HSQUIRRELVM v, const HSQOBJECT *a, const HSQOBJECT *b, SQInteger *res);
+
+SQUIRREL_API SQRESULT sq_direct_getuserdata(const HSQOBJECT *obj, SQUserPointer *p, SQUserPointer *typetag=NULL);
+
+#endif /* _SQDIRECT_H_ */
