134d9d943152eb064f5044ffd4946a685744abb5
VasiliyRyabtsev <vasiliy.ryabtsev@gmail.com>
2019-07-14 15:34:09 +0300

    Use FNV1a hash function for strings
    It works better than string hash from Lua 5 which in turn works
    better than original Squirrel hash taken from Lua 4
    (see https://github.com/albertodemichelis/squirrel/issues/83)

--- squirrel/sqstring.h
+++ squirrel/sqstring.h
@@ -2,14 +2,39 @@
 #ifndef _SQSTRING_H_
 #define _SQSTRING_H_
 
-inline SQHash _hashstr (const SQChar *s, size_t l)
+inline SQHash _hashstr_lua5(const SQChar *s, size_t l)
 {
         SQHash h = (SQHash)l;  /* seed */
-        size_t step = (l>>5)|1;  /* if string is too long, don't hash all its chars */
+        size_t step = (l>>5)+1;  /* if string is too long, don't hash all its chars */
         for (; l>=step; l-=step)
-            h = h ^ ((h<<5)+(h>>2)+(unsigned short)*(s++));
+            h ^= ((h<<5)+(h>>2)+(SQChar)(s[l-1]));
         return h;
 }
+//djb2
+inline SQHash _hashstr_djb2(const SQChar *s, size_t l)
+{
+  SQHash hash = SQHash(5381+l);
+  size_t step = (l>>5)+1;  /* if string is too long, don't hash all its chars */
+
+  for (; l>=step; l-=step)
+    hash = hash * 33 + s[l-1];
+
+  return hash;
+}
+
+//fnv1
+inline SQHash _hashstr_fnv1a(const SQChar *s, size_t l)
+{
+  SQHash result = 2166136261U;
+  size_t step = (l>>5)+1;  /* if string is too long, don't hash all its chars */
+
+  for (; l>=step; l-=step)
+    result = (result ^ s[l-1])* 16777619;
+  return result;
+}
+//__forceinline SQHash _hashstr (const SQChar *s, size_t l){return _hashstr_lua5(s, l);}//worst
+//__forceinline SQHash _hashstr (const SQChar *s, size_t l){return _hashstr_djb2(s, l);}//good
+__forceinline SQHash _hashstr (const SQChar *s, size_t l){return _hashstr_fnv1a(s, l);}
 
 struct SQString : public SQRefCounted
 {
