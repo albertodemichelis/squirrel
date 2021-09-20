albertodemichelis/squirrel

commit 2d842c174646461a31f5a5cadded3f6e9768fe55

Pass VM-specific context to every memory allocation function
Allow using allocator that is not only optimized for common size of
Squirrel objects but also non-thread-safe.
Useful for the case with multiple independent Squirel VMs in single application.

squirrel/sqobject.h
@@ -90,7 +90,7 @@ struct SQRefCounted
     struct SQWeakRef *_weakref;
     SQRefCounted() { _uiRef = 0; _weakref = NULL; }
     virtual ~SQRefCounted();
-    SQWeakRef *GetWeakRef(SQObjectType type);
+    SQWeakRef *GetWeakRef(SQAllocContext alloc_ctx, SQObjectType type);
     virtual void Release()=0;
 
 };
@@ -99,6 +99,7 @@ struct SQWeakRef : SQRefCounted
 {
     void Release();
     SQObject _obj;
+    SQAllocContext _alloc_ctx;
 };
 
 #define _realval(o) (sq_type((o)) != OT_WEAKREF?(SQObject)o:_weakref(o)->_obj)

squirrel/sqvm.h
@@ -131,7 +131,8 @@ public:
     }
     bool EnterFrame(SQInteger newbase, SQInteger newtop, bool tailcall);
     void LeaveFrame();
-    void Release(){ sq_delete(this,SQVM); }
+    void Release();
+
 ////////////////////////////////////////////////////////////////////////////
     //stack functions for the api
     void Remove(SQInteger n);

include/sqstdstring.h
@@ -8,13 +8,14 @@ extern "C" {
 
 typedef unsigned int SQRexBool;
 typedef struct SQRex SQRex;
+typedef struct SQAllocContextT * SQAllocContext;
 
 typedef struct {
     const SQChar *begin;
     SQInteger len;
 } SQRexMatch;
 
-SQUIRREL_API SQRex *sqstd_rex_compile(const SQChar *pattern,const SQChar **error);
+SQUIRREL_API SQRex *sqstd_rex_compile(SQAllocContext ctx, const SQChar *pattern,const SQChar **error);
 SQUIRREL_API void sqstd_rex_free(SQRex *exp);
 SQUIRREL_API SQBool sqstd_rex_match(SQRex* exp,const SQChar* text);
 SQUIRREL_API SQBool sqstd_rex_search(SQRex* exp,const SQChar* text, const SQChar** out_begin, const SQChar** out_end);

include/squirrel.h
@@ -363,10 +363,14 @@ SQUIRREL_API SQRESULT sq_resurrectunreachable(HSQUIRRELVM v);
 SQUIRREL_API SQRESULT sq_writeclosure(HSQUIRRELVM vm,SQWRITEFUNC writef,SQUserPointer up);
 SQUIRREL_API SQRESULT sq_readclosure(HSQUIRRELVM vm,SQREADFUNC readf,SQUserPointer up);
 
+
+typedef struct SQAllocContextT * SQAllocContext;
+SQUIRREL_API SQAllocContext sq_getallocctx(HSQUIRRELVM v);
+
 /*mem allocation*/
-SQUIRREL_API void *sq_malloc(SQUnsignedInteger size);
-SQUIRREL_API void *sq_realloc(void* p,SQUnsignedInteger oldsize,SQUnsignedInteger newsize);
-SQUIRREL_API void sq_free(void *p,SQUnsignedInteger size);
+SQUIRREL_API void *sq_malloc(SQAllocContext ctx, SQUnsignedInteger size);
+SQUIRREL_API void *sq_realloc(SQAllocContext ctx, void* p,SQUnsignedInteger oldsize,SQUnsignedInteger newsize);
+SQUIRREL_API void sq_free(SQAllocContext ctx, void *p,SQUnsignedInteger size);
 
 /*debug*/
 SQUIRREL_API SQRESULT sq_stackinfos(HSQUIRRELVM v,SQInteger level,SQStackInfos *si);

sqstdlib/sqstdblob.cpp
@@ -123,8 +123,9 @@ static SQInteger _blob__typeof(HSQUIRRELVM v)
 static SQInteger _blob_releasehook(SQUserPointer p, SQInteger SQ_UNUSED_ARG(size))
 {
     SQBlob *self = (SQBlob*)p;
+    SQAllocContext alloc_ctx = self->_alloc_ctx;
     self->~SQBlob();
-    sq_free(self,sizeof(SQBlob));
+    sq_free(alloc_ctx, self, sizeof(SQBlob));
     return 1;
 }
 
@@ -138,10 +139,11 @@ static SQInteger _blob_constructor(HSQUIRRELVM v)
     if(size < 0) return sq_throwerror(v, _SC("cannot create blob with negative size"));
     //SQBlob *b = new SQBlob(size);
 
-    SQBlob *b = new (sq_malloc(sizeof(SQBlob)))SQBlob(size);
+    SQAllocContext alloc_ctx = sq_getallocctx(v);
+    SQBlob *b = new (sq_malloc(alloc_ctx, sizeof(SQBlob)))SQBlob(alloc_ctx, size);
     if(SQ_FAILED(sq_setinstanceup(v,1,b))) {
         b->~SQBlob();
-        sq_free(b,sizeof(SQBlob));
+        sq_free(alloc_ctx,b,sizeof(SQBlob));
         return sq_throwerror(v, _SC("cannot create blob"));
     }
     sq_setreleasehook(v,1,_blob_releasehook);
@@ -156,11 +158,12 @@ static SQInteger _blob__cloned(HSQUIRRELVM v)
             return SQ_ERROR;
     }
     //SQBlob *thisone = new SQBlob(other->Len());
-    SQBlob *thisone = new (sq_malloc(sizeof(SQBlob)))SQBlob(other->Len());
+    SQAllocContext alloc_ctx = sq_getallocctx(v);
+    SQBlob *thisone = new (sq_malloc(alloc_ctx,sizeof(SQBlob))) SQBlob(alloc_ctx, other->Len());
     memcpy(thisone->GetBuf(),other->GetBuf(),thisone->Len());
     if(SQ_FAILED(sq_setinstanceup(v,1,thisone))) {
         thisone->~SQBlob();
-        sq_free(thisone,sizeof(SQBlob));
+        sq_free(alloc_ctx,thisone,sizeof(SQBlob));
         return sq_throwerror(v, _SC("cannot clone blob"));
     }
     sq_setreleasehook(v,1,_blob_releasehook);

sqstdlib/sqstdblobimpl.h
@@ -4,16 +4,18 @@
 
 struct SQBlob : public SQStream
 {
-    SQBlob(SQInteger size) {
+    SQBlob(SQAllocContext alloc_ctx, SQInteger size)
+        : _alloc_ctx(alloc_ctx)
+    {
         _size = size;
         _allocated = size;
-        _buf = (unsigned char *)sq_malloc(size);
+        _buf = (unsigned char *)sq_malloc(_alloc_ctx, size);
         memset(_buf, 0, _size);
         _ptr = 0;
         _owns = true;
     }
     virtual ~SQBlob() {
-        sq_free(_buf, _allocated);
+        sq_free(_alloc_ctx, _buf, _allocated);
     }
     SQInteger Write(void *buffer, SQInteger size) {
         if(!CanAdvance(size)) {
@@ -37,13 +39,13 @@ struct SQBlob : public SQStream
     bool Resize(SQInteger n) {
         if(!_owns) return false;
         if(n != _allocated) {
-            unsigned char *newbuf = (unsigned char *)sq_malloc(n);
+            unsigned char *newbuf = (unsigned char *)sq_malloc(_alloc_ctx, n);
             memset(newbuf,0,n);
             if(_size > n)
                 memcpy(newbuf,_buf,n);
             else
                 memcpy(newbuf,_buf,_size);
-            sq_free(_buf,_allocated);
+            sq_free(_alloc_ctx, _buf,_allocated);
             _buf=newbuf;
             _allocated = n;
             if(_size > _allocated)
@@ -97,6 +99,10 @@ struct SQBlob : public SQStream
     SQInteger Tell() { return _ptr; }
     SQInteger Len() { return _size; }
     SQUserPointer GetBuf(){ return _buf; }
+
+public:
+    SQAllocContext _alloc_ctx;
+
 private:
     SQInteger _size;
     SQInteger _allocated;

sqstdlib/sqstdio.cpp
@@ -61,8 +61,16 @@ SQInteger sqstd_feof(SQFILE file)
 
 //File
 struct SQFile : public SQStream {
-    SQFile() { _handle = NULL; _owns = false;}
-    SQFile(SQFILE file, bool owns) { _handle = file; _owns = owns;}
+    SQFile(SQAllocContext alloc_ctx)
+        : _alloc_ctx(alloc_ctx)
+        , _handle(nullptr)
+        , _owns(false)
+    {}
+    SQFile(SQAllocContext alloc_ctx, SQFILE file, bool owns)
+        : _alloc_ctx(alloc_ctx)
+        , _handle(file)
+        , _owns(owns)
+    {}
     virtual ~SQFile() { Close(); }
     bool Open(const SQChar *filename ,const SQChar *mode) {
         Close();
@@ -104,6 +112,10 @@ struct SQFile : public SQStream {
     bool IsValid() { return _handle?true:false; }
     bool EOS() { return Tell()==Len()?true:false;}
     SQFILE GetHandle() {return _handle;}
+
+public:
+    SQAllocContext _alloc_ctx;
+
 private:
     SQFILE _handle;
     bool _owns;
@@ -118,8 +130,9 @@ static SQInteger _file__typeof(HSQUIRRELVM v)
 static SQInteger _file_releasehook(SQUserPointer p, SQInteger SQ_UNUSED_ARG(size))
 {
     SQFile *self = (SQFile*)p;
+    SQAllocContext alloc_ctx = self->_alloc_ctx;
     self->~SQFile();
-    sq_free(self,sizeof(SQFile));
+    sq_free(alloc_ctx, self,sizeof(SQFile));
     return 1;
 }
 
@@ -141,10 +154,12 @@ static SQInteger _file_constructor(HSQUIRRELVM v)
         return sq_throwerror(v,_SC("wrong parameter"));
     }
 
-    f = new (sq_malloc(sizeof(SQFile)))SQFile(newf,owns);
+    SQAllocContext alloc_ctx = sq_getallocctx(v);
+
+    f = new (sq_malloc(alloc_ctx, sizeof(SQFile)))SQFile(alloc_ctx,newf,owns);
     if(SQ_FAILED(sq_setinstanceup(v,1,f))) {
         f->~SQFile();
-        sq_free(f,sizeof(SQFile));
+        sq_free(alloc_ctx,f,sizeof(SQFile));
         return sq_throwerror(v, _SC("cannot create blob with negative size"));
     }
     sq_setreleasehook(v,1,_file_releasehook);

sqstdlib/sqstdrex.cpp
@@ -53,6 +53,7 @@ typedef struct tagSQRexNode{
 }SQRexNode;
 
 struct SQRex{
+    SQAllocContext _alloc_ctx;
     const SQChar *_eol;
     const SQChar *_bol;
     const SQChar *_p;
@@ -80,7 +81,7 @@ static SQInteger sqstd_rex_newnode(SQRex *exp, SQRexNodeType type)
     if(exp->_nallocated < (exp->_nsize + 1)) {
         SQInteger oldsize = exp->_nallocated;
         exp->_nallocated *= 2;
-        exp->_nodes = (SQRexNode *)sq_realloc(exp->_nodes, oldsize * sizeof(SQRexNode) ,exp->_nallocated * sizeof(SQRexNode));
+        exp->_nodes = (SQRexNode *)sq_realloc(exp->_alloc_ctx,exp->_nodes, oldsize * sizeof(SQRexNode) ,exp->_nallocated * sizeof(SQRexNode));
     }
     exp->_nodes[exp->_nsize++] = n;
     SQInteger newid = exp->_nsize - 1;
@@ -551,19 +552,20 @@ static const SQChar *sqstd_rex_matchnode(SQRex* exp,SQRexNode *node,const SQChar
 }
 
 /* public api */
-SQRex *sqstd_rex_compile(const SQChar *pattern,const SQChar **error)
+SQRex *sqstd_rex_compile(SQAllocContext alloc_ctx, const SQChar *pattern,const SQChar **error)
 {
-    SQRex * volatile exp = (SQRex *)sq_malloc(sizeof(SQRex)); // "volatile" is needed for setjmp()
+    SQRex * volatile exp = (SQRex *)sq_malloc(alloc_ctx, sizeof(SQRex)); // "volatile" is needed for setjmp()
+    exp->_alloc_ctx = alloc_ctx;
     exp->_eol = exp->_bol = NULL;
     exp->_p = pattern;
     exp->_nallocated = (SQInteger)scstrlen(pattern) * sizeof(SQChar);
-    exp->_nodes = (SQRexNode *)sq_malloc(exp->_nallocated * sizeof(SQRexNode));
+    exp->_nodes = (SQRexNode *)sq_malloc(alloc_ctx, exp->_nallocated * sizeof(SQRexNode));
     exp->_nsize = 0;
     exp->_matches = 0;
     exp->_nsubexpr = 0;
     exp->_first = sqstd_rex_newnode(exp,OP_EXPR);
     exp->_error = error;
-    exp->_jmpbuf = sq_malloc(sizeof(jmp_buf));
+    exp->_jmpbuf = sq_malloc(alloc_ctx, sizeof(jmp_buf));
     if(setjmp(*((jmp_buf*)exp->_jmpbuf)) == 0) {
         SQInteger res = sqstd_rex_list(exp);
         exp->_nodes[exp->_first].left = res;
@@ -586,7 +588,7 @@ SQRex *sqstd_rex_compile(const SQChar *pattern,const SQChar **error)
             scprintf(_SC("\n"));
         }
 #endif
-        exp->_matches = (SQRexMatch *) sq_malloc(exp->_nsubexpr * sizeof(SQRexMatch));
+        exp->_matches = (SQRexMatch *) sq_malloc(alloc_ctx, exp->_nsubexpr * sizeof(SQRexMatch));
         memset(exp->_matches,0,exp->_nsubexpr * sizeof(SQRexMatch));
     }
     else{
@@ -599,10 +601,10 @@ SQRex *sqstd_rex_compile(const SQChar *pattern,const SQChar **error)
 void sqstd_rex_free(SQRex *exp)
 {
     if(exp) {
-        if(exp->_nodes) sq_free(exp->_nodes,exp->_nallocated * sizeof(SQRexNode));
-        if(exp->_jmpbuf) sq_free(exp->_jmpbuf,sizeof(jmp_buf));
-        if(exp->_matches) sq_free(exp->_matches,exp->_nsubexpr * sizeof(SQRexMatch));
-        sq_free(exp,sizeof(SQRex));
+        if(exp->_nodes) sq_free(exp->_alloc_ctx, exp->_nodes,exp->_nallocated * sizeof(SQRexNode));
+        if(exp->_jmpbuf) sq_free(exp->_alloc_ctx, exp->_jmpbuf,sizeof(jmp_buf));
+        if(exp->_matches) sq_free(exp->_alloc_ctx, exp->_matches,exp->_nsubexpr * sizeof(SQRexMatch));
+        sq_free(exp->_alloc_ctx, exp, sizeof(SQRex));
     }
 }
 

commit aa79fb1ab55af5cea20b876150a2816269a4c862

Fix to compile without garbage collector
Was broken in https://github.com/GaijinEntertainment/quirrel/commit/2d842c174646461a31f5a5cadded3f6e9768fe55

squirrel/sqobject.h
@@ -347,10 +347,17 @@ struct SQCollectable : public SQRefCounted {
 #define INIT_CHAIN() {_next=NULL;_prev=NULL;_sharedstate=ss;}
 #else
 
+// Need this to keep SQSharedState pointer to access alloc_ctx
+// Otherwise, just use SQRefCounted as CHAINABLE_OBJ in the way it was initially
+struct SQRefCountedWithSharedState : public SQRefCounted
+{
+    SQSharedState *_sharedstate;
+};
 #define ADD_TO_CHAIN(chain,obj) ((void)0)
 #define REMOVE_FROM_CHAIN(chain,obj) ((void)0)
-#define CHAINABLE_OBJ SQRefCounted
-#define INIT_CHAIN() ((void)0)
+#define CHAINABLE_OBJ SQRefCountedWithSharedState
+#define INIT_CHAIN() {_sharedstate=ss;}
+
 #endif
 
 struct SQDelegable : public CHAINABLE_OBJ {

squirrel/sqvm.h
@@ -202,7 +202,9 @@ inline SQObjectPtr &stack_get(HSQUIRRELVM v,SQInteger idx){return ((idx>=0)?(v->
 #ifndef NO_GARBAGE_COLLECTOR
 #define _opt_ss(_vm_) (_vm_)->_sharedstate
 #else
-#define _opt_ss(_vm_) NULL
+// This change is needed to make use of alloc_ctx
+// Without alloc_ctx NULL can be returned
+#define _opt_ss(_vm_) (_vm_)->_sharedstate
 #endif
 
 #define PUSH_CALLINFO(v,nci){ \