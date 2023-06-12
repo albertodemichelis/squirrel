albertodemichelis/squirrel

commit 561b428a6ceb2fb789f69da2c996be7a5f238da7

Drop support for implicit access to 'this' and root table
It makes script code unambiguous and straightforward for human,
compiler and static analyzer.
Explicit is better than implicit.

static_analyzer/quirrel_static_analyzer.cpp
@@ -606,7 +606,7 @@ bool find_ident_in_collected(CompilationContext & ctx,
   {
     order[0] = &const_visible_after_requires;
     order[1] = &local_visible_after_requires;
-    order[2] = (ctx.langFeatures & LF_EXPLICIT_ROOT_LOOKUP) ? &root_visible_after_requires : nullptr;
+    order[2] = nullptr;
   }
   else
     return false;
@@ -4101,6 +4101,11 @@ public:
           ignore = true;
         }
 
+        if ((it->second.declContext == DC_CLASS_MEMBER || it->second.declContext == DC_CLASS_METHOD))
+        {
+          ignore = true;
+        }
+
         if (!ignore)
         {
           ctx.warning("ident-hides-ident", var->tok, declContextToString(decl_context), name,
@@ -4190,8 +4195,7 @@ public:
       if (it != localIdentifiers[i].end())
       {
         if (it->second.declContext == DC_TABLE_MEMBER || it->second.declContext == DC_CLASS_MEMBER)
-          if (i == from_scope || (ctx.langFeatures & LF_EXPLICIT_THIS))
-            continue;
+          continue;
 
         return it->second.declContext;
       }
@@ -4245,20 +4249,11 @@ public:
         break;
       }
 
-    auto it = globalTables.find(currentClass);
-    if (it != globalTables.end())
-      if (!(ctx.langFeatures & LF_EXPLICIT_THIS) && it->second.find(parent) != it->second.end())
-        return DC_CLASS_MEMBER;
-
     if (globalIdentifiers.find(parent) != globalIdentifiers.end())
       return DC_GLOBAL_VARIABLE;
 
     if (globalConsts.find(parent) != globalConsts.end())
       return DC_GLOBAL_CONSTANT;
-
-    if (!(ctx.langFeatures & LF_EXPLICIT_THIS))
-      if (is_ident_visible(parent, nodePath))
-        return DC_IDENTIFIER;
 
     for (size_t i = nodePath.size() - 1; nodePath[i] != nullptr; i--)
       if (nodePath[i]->nodeType == PNT_FUNCTION || nodePath[i]->nodeType == PNT_CLASS_MEMBER)

squirrel/sqastcodegen.cpp
@@ -1586,8 +1586,7 @@ void CodegenVisitor::visitId(Id *id) {
 
         */
         // TODO: probably we need a special handling for some corner cases
-        if ((_fs->lang_features & LF_EXPLICIT_THIS)
-            && !(_fs->lang_features & LF_TOOLS_COMPILE_CHECK))
+        if (!(_fs->lang_features & LF_TOOLS_COMPILE_CHECK))
             error(id, _SC("Unknown variable [%s]"), _stringval(idObj));
 
         _fs->PushTarget(0);

squirrel/sqastparser.cpp
@@ -68,18 +68,10 @@ void SQParser::ProcessDirective()
         setFlags = LF_STRICT_BOOL;
     else if (strcmp(sval, _SC("relaxed-bool")) == 0)
         clearFlags = LF_STRICT_BOOL;
-    else if (strcmp(sval, _SC("no-root-fallback")) == 0)
-        setFlags = LF_EXPLICIT_ROOT_LOOKUP;
-    else if (strcmp(sval, _SC("implicit-root-fallback")) == 0)
-        clearFlags = LF_EXPLICIT_ROOT_LOOKUP;
     else if (strcmp(sval, _SC("no-plus-concat")) == 0)
         setFlags = LF_NO_PLUS_CONCAT;
     else if (strcmp(sval, _SC("allow-plus-concat")) == 0)
         clearFlags = LF_NO_PLUS_CONCAT;
-    else if (strcmp(sval, _SC("explicit-this")) == 0)
-        setFlags = LF_EXPLICIT_THIS;
-    else if (strcmp(sval, _SC("implicit-this")) == 0)
-        clearFlags = LF_EXPLICIT_THIS;
     else if (strcmp(sval, _SC("forbid-root-table")) == 0)
         setFlags = LF_FORBID_ROOT_TABLE;
     else if (strcmp(sval, _SC("allow-root-table")) == 0)

squirrel/sqcompiler.cpp
@@ -149,18 +149,10 @@ public:
             setFlags = LF_STRICT_BOOL;
         else if (strcmp(sval, _SC("relaxed-bool")) == 0)
             clearFlags = LF_STRICT_BOOL;
-        else if (strcmp(sval, _SC("no-root-fallback")) == 0)
-            setFlags = LF_EXPLICIT_ROOT_LOOKUP;
-        else if (strcmp(sval, _SC("implicit-root-fallback")) == 0)
-            clearFlags = LF_EXPLICIT_ROOT_LOOKUP;
         else if (strcmp(sval, _SC("no-plus-concat")) == 0)
             setFlags = LF_NO_PLUS_CONCAT;
         else if (strcmp(sval, _SC("allow-plus-concat")) == 0)
             clearFlags = LF_NO_PLUS_CONCAT;
-        else if (strcmp(sval, _SC("explicit-this")) == 0)
-            setFlags = LF_EXPLICIT_THIS;
-        else if (strcmp(sval, _SC("implicit-this")) == 0)
-            clearFlags = LF_EXPLICIT_THIS;
         else if (strcmp(sval, _SC("forbid-root-table")) == 0)
             setFlags = LF_FORBID_ROOT_TABLE;
         else if (strcmp(sval, _SC("allow-root-table")) == 0)
@@ -1062,8 +1054,7 @@ public:
                 * for the latter. If we are not using the variable as a dref expr, generate
                 * the _OP_GET instruction.
                 */
-                if ((_fs->lang_features & LF_EXPLICIT_THIS)
-                    && !(_fs->lang_features & LF_TOOLS_COMPILE_CHECK))
+                if (!(_fs->lang_features & LF_TOOLS_COMPILE_CHECK))
                     Error(_SC("Unknown variable [%s]"), _stringval(id));
 
                 _fs->PushTarget(0);

squirrel/sqfuncproto.h
@@ -10,19 +10,20 @@ enum SQOuterType {
 };
 
 enum SQLangFeature {
-    LF_STRICT_BOOL = 0x01,
-    LF_EXPLICIT_ROOT_LOOKUP = 0x02,
-    LF_NO_PLUS_CONCAT = 0x10,
-    LF_EXPLICIT_THIS = 0x20,
-    LF_FORBID_ROOT_TABLE = 0x40,
-    LF_TOOLS_COMPILE_CHECK = 0x1000,
-    LF_DISABLE_OPTIMIZER = 0x2000,
-    LF_FORBID_GLOBAL_CONST_REWRITE = 0x4000,
+    // parsing stage
+    LF_FORBID_ROOT_TABLE = 0x1,
+
+    // code generation stage
+    LF_TOOLS_COMPILE_CHECK = 0x10,
+    LF_DISABLE_OPTIMIZER = 0x20,
+    LF_FORBID_GLOBAL_CONST_REWRITE = 0x40,
+
+    // runtime stage
+    LF_STRICT_BOOL = 0x80,
+    LF_NO_PLUS_CONCAT = 0x100,
 
     LF_STRICT = LF_STRICT_BOOL |
-                LF_EXPLICIT_ROOT_LOOKUP |
                 LF_NO_PLUS_CONCAT |
-                LF_EXPLICIT_THIS |
                 LF_FORBID_ROOT_TABLE
 };
 

squirrel/sqstate.cpp
@@ -15,7 +15,7 @@
 SQSharedState::SQSharedState(SQAllocContext allocctx) :
     _alloc_ctx(allocctx),
     _refs_table(allocctx),
-    defaultLangFeatures(LF_EXPLICIT_ROOT_LOOKUP | LF_EXPLICIT_THIS)
+    defaultLangFeatures(0)
 {
     _compilererrorhandler = NULL;
     _printfunc = NULL;

squirrel/sqvm.cpp
@@ -1778,19 +1778,6 @@ SQInteger SQVM::GetImpl(const SQObjectPtr &self, const SQObjectPtr &key, SQObjec
             return SLOT_RESOLVE_STATUS_OK;
         }
     }
-//#ifdef ROOT_FALLBACK
-    if(selfidx == 0 && sq_isclosure(ci->_closure) && !(_closure(ci->_closure)->_function->lang_features & LF_EXPLICIT_ROOT_LOOKUP)) {
-        SQWeakRef *w = _closure(ci->_closure)->_root;
-        if(sq_type(w->_obj) != OT_NULL)
-        {
-            if(Get(*((const SQObjectPtr *)&w->_obj),key,dest,0,DONT_FALL_BACK)) {
-                propagate_immutable(self, dest);
-                return SLOT_RESOLVE_STATUS_OK;
-            }
-        }
-
-    }
-//#endif
     if ((getflags & GET_FLAG_DO_NOT_RAISE_ERROR) == 0) Raise_IdxError(key);
     return SLOT_RESOLVE_STATUS_NO_MATCH;
 }

static_analyzer/compilation_context.h
@@ -32,17 +32,13 @@ enum ErrorLevels
 enum SQLangFeature
 {
   LF_STRICT_BOOL = 0x01,
-  LF_EXPLICIT_ROOT_LOOKUP = 0x02,
   LF_NO_PLUS_CONCAT = 0x10,
-  LF_EXPLICIT_THIS = 0x20,
   LF_FORBID_ROOT_TABLE = 0x40,
   LF_TOOLS_COMPILE_CHECK = 0x1000,
   LF_DISABLE_OPTIMIZER = 0x2000,
 
   LF_STRICT = LF_STRICT_BOOL |
-              LF_EXPLICIT_ROOT_LOOKUP |
               LF_NO_PLUS_CONCAT |
-              LF_EXPLICIT_THIS |
               LF_FORBID_ROOT_TABLE
 };
 

commit 3ad1813f133de56bdb9f2c93ec9c72421ab35a3b

Static analyzer: fix regression (after commit about explicit this)
Was broken in 561b428a6ceb2fb789f69da2c996be7a5f238da7

static_analyzer/quirrel_static_analyzer.cpp
@@ -4101,11 +4101,6 @@ public:
           ignore = true;
         }
 
-        if ((it->second.declContext == DC_CLASS_MEMBER || it->second.declContext == DC_CLASS_METHOD))
-        {
-          ignore = true;
-        }
-
         if (!ignore)
         {
           ctx.warning("ident-hides-ident", var->tok, declContextToString(decl_context), name,
@@ -4195,6 +4190,7 @@ public:
       if (it != localIdentifiers[i].end())
       {
         if (it->second.declContext == DC_TABLE_MEMBER || it->second.declContext == DC_CLASS_MEMBER)
+          if (i == from_scope)
           continue;
 
         return it->second.declContext;
@@ -4254,6 +4250,9 @@ public:
 
     if (globalConsts.find(parent) != globalConsts.end())
       return DC_GLOBAL_CONSTANT;
+
+    if (is_ident_visible(parent, nodePath))
+      return DC_IDENTIFIER;
 
     for (size_t i = nodePath.size() - 1; nodePath[i] != nullptr; i--)
       if (nodePath[i]->nodeType == PNT_FUNCTION || nodePath[i]->nodeType == PNT_CLASS_MEMBER)