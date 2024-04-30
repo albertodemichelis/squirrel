418cb1cae16fde0df8a42e56eeb283f8288875d5
Emmanuel Aubertin <aubertin.emmanuel@gmail.com>
2024-04-30 23:37:17 +0200

    🚀 --install option 🚀

--- build.sh
+++ build.sh
@@ -1,12 +1,62 @@
 #!/bin/sh
 
+TITLE="Squirrel-lang installer"
+PROGNAME=$(basename $0)
+RELEASE="Revision 1.0"
+AUTHOR="(c) 2024 celisej567 & Aubertin Emmanuel (aka aTHO)"
+INSTALL=0
+
+
+print_release() {
+    echo "$RELEASE $AUTHOR"
+}
+
+print_usage() {
+        echo ""
+        echo "$TITLE"
+        echo ""
+        echo "Usage: ./$PROGNAME | [-h | --help] | [-v | --version] | [-i | --install]"
+        echo ""
+		echo "          -h, --help		Help message"
+		echo "          -v, --version		Version"
+        echo "          -i, --install		Install all binaries on your computer (in /bin)"
+        echo ""
+}
+
+print_help() {
+        echo ""
+        print_usage
+        echo ""
+        echo ""
+		print_release $PROGNAME $RELEASE
+		exit 0
+}
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -h | --help)
+            print_help
+            exit 0
+            ;;
+        -v | --version)
+                print_release
+                exit 0
+                ;;
+        -i | --install)
+                INSTALL=1
+                ;;
+        *)  echo "Unknow argument: $1"
+            print_usage
+            ;;
+        esac
+shift
+done
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
 if [ -d "build" ]; then
-	cd build
-	
 	if [ -z "$1" ]; then
 		echo "Building with Release Config."
 		config="release"
@@ -14,14 +64,26 @@ if [ -d "build" ]; then
 		echo "Building with $1 Config."
 		config="$1"
 	fi
-
-	cmake --build . --config "$config"
-	if [ $? -eq 0 ]; then
-		echo "${GREEN}Libraries built and should be located in \"/build/bin/\" folder.${NC}"
-	else
-		echo "${RED}Error: Build failed.${NC}"
-	fi
-
 else
-	echo "\"build\" folder does not exist. Call \"configure.sh\" to configure it."
+	mkdir build -p
+
+	if [ -d "build" ]; then
+		cd build 
+		cmake .. $*
+	else 
+		echo "Unable to create \"build\" folder. Stopping configuration. \n"
+	fi
+fi
+
+cd build
+
+cmake --build . --config "$config"
+if [ $? -eq 0 ]; then
+	echo "${GREEN}Libraries built and should be located in \"/build/bin/\" folder.${NC}"
+	if [ $INSTALL  -eq 1 ]; then
+		sudo cp ./bin/sq /bin/sq
+		echo "${GREEN}Libraries installed, feel to try sq or sq_static.${NC}"
+	fi
+else
+	echo "${RED}Error: Build failed.${NC}"
 fi
