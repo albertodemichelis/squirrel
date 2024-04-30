60f1c6195012f258b8f03b016085957509a66a2a
Emmanuel Aubertin <aubertin.emmanuel@gmail.com>
2024-04-30 23:12:25 +0200

    😎 Add some colors 😎

--- build.sh
+++ build.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 if [ -d "build" ]; then
 	cd build
+	
 	if [ -z "$1" ]; then
-		echo Building with Release Config.
-		cmake --build . --config release
+		echo "Building with Release Config."
+		config="release"
 	else
-		echo Building with $1 Config.
-		cmake --build . --config $1
+		echo "Building with $1 Config."
+		config="$1"
 	fi
 
-	echo "Libraries builded and should be located in \"/build/bin/\" folder. \n"
-else 
-	echo "\"build\" folder does not exist. Call \"configure.sh\" to configure it. \n"
+	cmake --build . --config "$config"
+	if [ $? -eq 0 ]; then
+		echo "${GREEN}Libraries built and should be located in \"/build/bin/\" folder.${NC}"
+	else
+		echo "${RED}Error: Build failed.${NC}"
+	fi
+
+else
+	echo "\"build\" folder does not exist. Call \"configure.sh\" to configure it."
 fi
